ffee9f954d426b23a3b182f070eb4589a61c3f21
Anatolii Utochkin <clashertolia@gmail.com>
2025-09-25 12:54:37 +0200

    Remove auto-generated assembly info and cache files from GameStore.Api

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425f2ec6d785976c191b6da26df26df33d1e1748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acebc37da9f12205012a43ea4c64b500a8a2e544")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
