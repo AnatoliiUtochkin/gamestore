7618c4c9dfdfd2a22a0701f9d94d8487f0c1765d
Anatolii Utochkin <clashertolia@gmail.com>
2025-09-26 21:32:38 +0200

    Minor changes

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b2df3a259e2dee439c9e2b106517a9ba61aeed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337b77b27b1b1578ba09fdd7629b20416eaea081")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
